d1a48b2db29cf8b626cdff3ecfafff9555bafb84
lrc <2691936627@qq.com>
2023-12-12 15:03:26 +0800

    reference and python code

--- davidson_diag.py
+++ davidson_diag.py
@@ -3,7 +3,7 @@ import scipy
 from scipy import linalg
 import time
 
-N = 10000
+N = 15000
 
 # construct a real symmetric matrix
 sparsity = 0.0001
@@ -14,7 +14,8 @@ A = A + sparsity*np.random.randn(N,N)
 A = (A.T + A)/2
 
 k = 4# number of eigenvalues
-l = 6# l >= k
+dimsub = 6# initial dim of subspace >=k
+l = dimsub
 B = np.eye(N,l)
 theta = np.zeros(N)
 I = np.eye(N)
@@ -22,16 +23,16 @@ I = np.eye(N)
 T1 = time.perf_counter()
 for a in range(N//2):
   _A_ = np.einsum('ki,kl,lj->ij',B,A,B)# most expensive
-  Value,Coe = linalg.eigh(_A_)# dim of subspace is l
+  Value,Coe = linalg.eigh(_A_)
   q = np.einsum('ij,jk,k->i',(A-Value[k-1]*I),B,Coe[:,k-1])
   norm_q = np.linalg.norm(q)
-  if norm_q<1e-8 and l!=8 : break
+  if norm_q<1e-8 and l!=dimsub : break
   for i in range(N):
     theta[i] = q[i]/(Value[k-1]-A[i,i])
   B = np.insert(B,l,theta,axis=1)
   B,R = np.linalg.qr(B)
   l = l+1
-print(Value[k-1],'\n',l-8)
+print(Value[k-1],'\n',l-dimsub)
 T2 = time.perf_counter()
 
 T3 = time.perf_counter()
